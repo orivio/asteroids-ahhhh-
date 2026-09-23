2073ec9dfef7e0c5a5d0bf433dc75bee75d187fc
Russlan Hossain <84582272+orivio@users.noreply.github.com>
2026-09-22 20:26:08 -0400

    added basic asteroids

---
+++ script.py
@@ -0,0 +1,48 @@
+import pygame
+import random
+
+
+pygame.init()
+screen = pygame.display.set_mode((600, 400))
+clock = pygame.time.Clock()
+
+
+class asteroid:
+    def __init__(self):
+        self.vec = pygame.math.Vector2(random.randint(100, 500), random.randint(100, 300))
+        self.vecmove = pygame.math.Vector2(random.randint(1, 3), random.randint(1, 3))
+        self.radius = random.randint(10, 30)
+
+
+gameloop = True
+
+asteroids = [asteroid(), asteroid(), asteroid(), asteroid(), asteroid(), asteroid()]
+for ast in asteroids:
+    for other in asteroids:
+        if ast.vec == other.vec:
+            ast = asteroid()
+
+
+while gameloop:
+    for event in pygame.event.get():
+        if event == pygame.QUIT:
+            gameloop = False
+    screen.fill((54, 33, 12))
+    for asteroid in asteroids:
+        pygame.draw.circle(screen, ((88, 149, 173)), (asteroid.vec.x, asteroid.vec.y), asteroid.radius)
+        asteroid.vec += asteroid.vecmove
+        if asteroid.vec.x + asteroid.radius > 600 or 0 > asteroid.vec.x - asteroid.radius:
+            asteroid.vecmove.x *= -1 
+        if asteroid.vec.y + asteroid.radius > 400 or 0 > asteroid.vec.y - asteroid.radius:
+            asteroid.vecmove.y *= -1
+        for other in asteroids:
+            if asteroid.vec.distance_to(other.vec) <= asteroid.radius + other.radius:
+                asteroid.vecmove = -asteroid.vecmove
+                other.vecmove = -other.vecmove
+
+    
+    clock.tick(30)
+    pygame.display.flip()
+
+
+pygame.quit()
